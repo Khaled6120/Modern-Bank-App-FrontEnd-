9db191856defaad60dec2c99e9d9a69c9f5724a3
Khaled6120 <alnaes1234@gmail.com>
2023-03-03 22:23:31 +0300

    add: initial files

---
+++ src/components/CardDeal.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import { card} from '../assets'
+import styles , {layout} from '../style'
+import Button from './Button'
+const CardDeal = () => {
+  return (
+    <section className={layout.section}>
+      <div className={layout.sectionInfo}>
+        <h2 className={styles.heading2}>Find a better card deal <br className='sm:block hidden' />in a few easy steps.</h2>
+        <p className={`${styles.paragraph} max-w-[470px] mt-5`}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus possimus veniam quisquam illum debitis dignissimos illo ratione quo? Quasi, cupiditate.</p>
+        <Button styles="mt-10" />
+      </div>
+
+      <div className={layout.sectionImg}>
+        <img src={card} alt="" className='w-[100%] h-[100%]' />
+      </div>
+
+
+    </section>
+  )
+}
+
+export default CardDeal
---
+++ src/components/index.js
@@ -0,0 +1,29 @@
+import Navbar from "./Navbar";
+import Billing from "./Billing";
+import CardDeal from "./CardDeal";
+import Business from "./Business";
+import Clients from "./Clients";
+import CTA from "./CTA";
+import Stats from "./Stats";
+import Footer from "./Footer";
+import Testimonials from "./Testimonials";
+import Hero from "./Hero";
+import Button from "./Button";
+import FeedbackCard from "./FeedbackCard";
+import GetStarted from "./GetStarted";
+
+export {
+  Navbar,
+  Billing,
+  CardDeal,
+  Business,
+  Clients,
+  CTA,
+  Stats,
+  Footer,
+  Testimonials,
+  Hero,
+  Button,
+  FeedbackCard,
+  GetStarted
+};
